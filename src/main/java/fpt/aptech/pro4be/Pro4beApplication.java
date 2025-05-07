26a38c960f02b37e34501c7dc0eecd5e4987fabf
tuann300724 <tuann300724@gmail.com>
2025-05-07 10:13:22 +0700

    first commit

---
+++ src/main/java/fpt/aptech/pro4be/Pro4beApplication.java
@@ -0,0 +1,13 @@
+package fpt.aptech.pro4be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro4beApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro4beApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpt/aptech/pro4be/Pro4beApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.aptech.pro4be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro4beApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
